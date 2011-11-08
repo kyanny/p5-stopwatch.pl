0ca25f8b2263a763c8c0f3d934366482a89c8aef
Kensuke Nagae <k-kaneko@paperboy.co.jp>
2011-11-08 22:25:37 +0900

    - code cleanup - add -v option

--- stopwatch.pl
+++ stopwatch.pl
@@ -3,15 +3,23 @@ use strict;
 use warnings;
 use Getopt::Long qw(HelpMessage);
 use Time::HiRes qw(gettimeofday tv_interval);
+use File::Basename;
+
+our $VERSION = '0.01';
 
 GetOptions(
-    '--initialize=s' => \my $initialize,
-    '--before=s'     => \my $before,
-    '--main=s'       => \my $main,
-    '--after=s'      => \my $after,
-    '--finalize=s'   => \my $finalize,
-    '--count=i'      => \my $count,
-    '--help'         => \&HelpMessage,
+    'initialize=s' => \my $initialize,
+    'before=s'     => \my $before,
+    'main=s'       => \my $main,
+    'after=s'      => \my $after,
+    'finalize=s'   => \my $finalize,
+    'count=i'      => \my $count,
+    'v|verbose+'   => \my $verbose,
+    'version'      => sub {
+        print basename($0) . '/' . $VERSION . "\n";
+        exit;
+    },
+    'help'         => \&HelpMessage,
 );
 unless ($main) {
     HelpMessage();
@@ -19,41 +27,82 @@ unless ($main) {
 unless ($count) {
     HelpMessage();
 }
+
+$verbose ||= 0;
+my $verbose_level_1 = $verbose > 0;
+my $verbose_level_2 = $verbose > 1;
+
 my $total;
 my $ret;
 
-print "Trying $count times... \n";
-if ($initialize) {
-    print "initializing...\n";
-    `$initialize`;
+#----
+sub initialize {
+    info('initializing...');
+
+    $ret = `$initialize`;
+    debug($ret);
 }
 
-for my $i (1..$count) {
-    if ($before) {
-        print "$i: run before hook...\n";
-        `$before`;
-    }
+sub before {
+    info('run before hook...');
 
+    $ret = `$before`;
+    debug($ret);
+}
+
+sub main {
     my $start = [gettimeofday()];
-    print "$i: run main...\n";
-    `$main`;
+    info('run main...');
+
+    $ret = `$main`;
+    debug($ret);
+
     my $end = [gettimeofday()];
     my $elapsed = tv_interval($start, $end);
-    print "$i: Elapsed: $elapsed\n";
+    print $elapsed, "\n";
+
     $total += $elapsed;
-    
-
-    if ($after) {
-        print "$i: run after hook...\n";
-        `$after`;
-    }
 }
 
-if ($finalize) {
-    print "finalizing...\n";
-    `$finalize`;
+sub after {
+    info('run after hook...');
+
+    $ret = `$after`;
+    debug($ret);
 }
+
+sub finalize {
+    info('finalizing...');
+
+    $ret = `$finalize`;
+    debug($ret);
+}
+
+#----
+sub info {
+    print @_, "\n" if $verbose_level_1;
+}
+
+sub debug {
+    print @_, "\n" if $verbose_level_2;
+}
+
+#----
+print "Trying $count times... \n";
+initialize() if $initialize;
+
+for my $i (1..$count) {
+    before() if $before;
+
+    print "==> $i/$count ... ";
+    main();
+
+    after() if $after;
+}
+
+finalize() if $finalize;
 print "Total: $total\n";
+
 
 __END__
 
@@ -64,12 +113,17 @@ stopwatch.pl -
 =head1 SYNOPSIS
 
   $ ./stopwatch.pl \
-      --initialize 'curl -O http://memcached.googlecode.com/files/memcached-1.4.9.tar.gz' \
+      --initialize 'curl -Os http://memcached.googlecode.com/files/memcached-1.4.9.tar.gz' \
       --before     'tar xzf memcached-1.4.9.tar.gz' \
       --main       'cd memcached-1.4.9 && ./configure && make' \
       --after      'cd memcached-1.4.9 && make clean' \
       --finalize   'rm -rf memcached-1.4.9 && rm memcached-1.4.9.tar.gz' \
       --count 10
+
+  Options:
+    --verbose
+    --version
+    --help
 
 =head1 DESCRIPTION
 
